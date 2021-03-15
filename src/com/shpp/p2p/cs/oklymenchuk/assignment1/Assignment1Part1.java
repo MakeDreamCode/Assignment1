404e76a6527e6ea3b654fabcb320ab088d55fe9e
MakeDreamCode <65960125+MakeDreamCode@users.noreply.github.com>
2021-03-15 18:23:58 +0200

    New version of Assignment1Part1. Very simple.

--- src/com/shpp/p2p/cs/oklymenchuk/assignment1/Assignment1Part1.java
+++ src/com/shpp/p2p/cs/oklymenchuk/assignment1/Assignment1Part1.java
@@ -10,94 +10,74 @@ public class Assignment1Part1 extends KarelTheRobot {
 
     /**
      * Runs the program. Must contains ONLY free methods.
-     * goForTheNewspaper() - Initialize Karel. Karel goes to a newspaper.
-     * pickupTheNewspaper() - Karel pickups the newspaper.
-     * backToStartWithTheNewspaper() - Karel returns to the start position with the newspaper.
+     * moveForNewspaper() - Initialize Karel. Karel goes to a newspaper.
+     * takeTheNewspaper() - Karel pickups the newspaper.
+     * returnToStartPosition() - Karel returns to the start position with the newspaper.
      */
     public void run() throws Exception {
-       goForTheNewspaper();
-       pickupTheNewspaper();
-       backToStartWithTheNewspaper();
-    }
-
-    /**
-     * Calculates a direction for nex step.
-     * Method checks left side of Karel and keeps Karel against the left wall
-     * for left-hand traffic.
-     */
-    private void calculateDirectionForNextStep() throws Exception {
-        if (leftIsClear()){                                 // if the left is clear, it means that Karel has reached the turn
-            turnLeft();                                     // and need to turn left
-        }
-        while (frontIsBlocked()) {                          // if the front is blocked Karel is looking for a free direction.
-                                                            // The wall should be on the left.
-            if(leftIsBlocked() && rightIsBlocked()){        // if Karel is at a dead end
-                turnBack();                                 // reversal for exit.
-            }
-            if (leftIsBlocked() && rightIsClear()) {        // if Karel is in the corner
-                turnRight();                                // must turn for exit
-            }
-        }
-    }
-
-    /**
-     * Moves Karel while Karel will not found the beeper.
-     */
-    private void go() throws Exception {
-        while (noBeepersPresent()){                         // beeper not found
-            oneStepForward();                               // make one step forward
-        }
-    }
-
-    /**
-     *Initializes and moves Karel to the newspaper.
-     */
-    private void goForTheNewspaper() throws Exception {
-        initialize();                                       // initialize Karel
-        go();                                               // move to the newspaper
+        moveForNewspaper();
+        takeTheNewspaper();
+        returnToStartPosition();
     }
 
     /**
      * Initialize Karel.
-     * Karel must say "Hello. I`m ready!" (switch on robot).
+     * Karel must say "Hello. Ready to work." (switch on robot).
      * Mark the place by installing a beeper to return.
-     * Turn around so that the wall is on the left side.
      * Make one step forward for get off from the beeper.
      */
-    private void initialize() throws Exception {
-        say("Hello. I`m ready!");        // switch on robot
+    private void karelInit() throws Exception {
+        say("Hello. Ready to work.");
         putBeeper();
-        while(leftIsClear()){
-            turnLeft();                                     // turn left until a wall is on the left side
+        move();
+    }
+
+
+    private void moveIfForwardFree() throws Exception {
+        while (frontIsBlocked()) {
+            turnRight();
         }
-        oneStepForward();                                   // get off from the beeper
+        move();
     }
 
-    /**
-     * Makes one step forward.
-     */
-    private void backToStartWithTheNewspaper() throws Exception {
-        turnBack();                                         // turn back
-        move();                                             // back to the door (wall must be at left side)
-        go();                                               // move to the start beeper
-        pickBeeper();                                       // pickup the start beeper
-        say("Job`s done!");
+
+    private void moveForNewspaper() throws Exception {
+
+        karelInit();
+
+        while (noBeepersPresent() && leftIsBlocked()) {
+            moveIfForwardFree();
+        }
     }
 
-    /**
-     * Makes one step forward.
-     */
-    public void oneStepForward() throws Exception {
-        calculateDirectionForNextStep();                    // looking for the right direction of movement
-        move();                                             // make one step forward
-    }
 
-    /**
-     * Pickups the newspaper.
-     */
-    private void pickupTheNewspaper() throws Exception {
-        say("The Newspaper is found!");
+    private void returnToStartPosition() throws Exception {
+        while (noBeepersPresent()) {
+            moveIfForwardFree();
+        }
+
         pickBeeper();
+        say("Job is finished!");
+    }
+
+
+    private void takeTheNewspaper() throws Exception {
+
+        turnLeft();
+
+        while (noBeepersPresent()){
+            move();
+        }
+
+        pickBeeper();
+        turnBack();
+
+        while (leftIsClear() && rightIsClear()){
+            move();
+        }
+
+        move();
+        turnLeft();
     }
 
     /**
