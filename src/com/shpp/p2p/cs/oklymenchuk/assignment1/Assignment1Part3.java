ef2fa37c6088189a073a024b0f3ab1391cbf3363
MakeDreamCode <65960125+MakeDreamCode@users.noreply.github.com>
2021-03-16 09:16:16 +0200

    All code rewritten: maximum simplification. Need to add comments in future.

--- src/com/shpp/p2p/cs/oklymenchuk/assignment1/Assignment1Part3.java
+++ src/com/shpp/p2p/cs/oklymenchuk/assignment1/Assignment1Part3.java
@@ -12,165 +12,85 @@ import com.shpp.karel.KarelTheRobot;
  * It doesn't matter where Karel looks after he finishes his race.
  *
  * Algorithm for solving the problem:
- * 1. Fill the entire field with beepers as a result of a spiral movement. The last free cell is on the normal
- * that passes through the center of the south line.
- * 2. Descend normal to the south line, put second beeper and return to the start position.
- * 3. Remove all beepers.
+
  */
 public class Assignment1Part3 extends KarelTheRobot{
 
     public void run() throws Exception {
-        initialize();                                       // switch on robot and look East
 
-        if (frontIsBlocked()) {                             // block 1х1
-            say("Block 1х1");
+        karelInit();
+
+        // для 1хч
+
+        if (frontIsBlocked()){
             putBeeper();
-        } else {                                            // normal mode
-            say("Block 2х2 or higher");
-            fillEverythingWithBeepers();                    //fill all cells beepers
-            returnToStartPositionAndPutBeeper();            // put second beeper and return to start(middle point has two beepers)
-            clearAllBeepers();                              // remove all beepers
-        }
-        say("Job`s done!");
-    }
+        } else {
 
-    /**
-     * Back to previous cell and turn back.
-     */
-    private void backToPreviousPosition() throws Exception {
-        turnBack();
-        move();
-        turnBack();
-    }
+            // заполнить нижний ряд биперами
+            while (frontIsClear()) {
+                putBeeper();
+                move();
 
-    /**
-     * Clear all beepers.
-     * Karel moves from West (start position) to East and pickups beepers.
-     * After the border is reached Karel goes at next line above,
-     * turns West and moves and pickups beepers to next border (West).
-     * <-<-<-<-
-     * ->->->->
-     */
-    private void clearAllBeepers() throws Exception {
-        while (frontIsClear()) {
-
-            pickBeeper();                                   // pickup a beeper if the front is clear
-
-            if (frontIsClear()) {
-                move();                                     // if the next cell is free, move to the next cell
-            }
-
-            if (frontIsBlocked() && facingEast()) {
-
-                if (beepersPresent()) {
-                    pickBeeper();                           // pickup a beeper if the last cell at East border has a beeper
-                }
-
-                    turnLeft();                             // look North
-
-                if (frontIsClear()) {
-                    move();                                 // move at the next free cell
-                    turnLeft();                             // look West
+                if (frontIsBlocked()) {
+                    putBeeper();
                 }
             }
 
-            if (frontIsBlocked() && facingWest()) {
+            //вернуться в начало
+            turnBack();
+            while (frontIsClear()) {
+                move();
+            }
+            turnBack();
 
-                if(beepersPresent()){
-                    pickBeeper();                           // pickup a beeper if the last cell at West border has a beeper
+
+            while (beepersPresent() && frontIsClear()) {
+
+                move();
+
+                // удаляем биперы с краев
+                if (frontIsBlocked() && beepersPresent()) {
+                    pickBeeper();
+                    turnBack();
+                    move();
                 }
 
-                turnRight();                                // look North
+                // удаляем биперы с краев
+                if (!beepersPresent()) {
 
-                if (frontIsClear()) {
-                    move();                                 // move at the next free cell
-                    turnRight();                            // look East
+                    //нет бипера? разворот и возврат на две клетки назад
+                    turnBack();
+                    move();
+                    move();
+
+                    // если в предпоследней от "края" клетке есть бипер
+                    if (beepersPresent()) {
+                        // развернулись и перешли в последнюю клетку с бипером
+                        turnBack();
+                        move();
+                        // подняли бипер, развернулись и перешли на "теперь уже последнюю" клетку с бипером
+                        pickBeeper();
+                        turnBack();
+                        move();
+                    }
                 }
             }
         }
-    }
 
-    /**
-     * Puts a beeper in each cell.
-     * Movement and puts beepers - along the perimeter of the rectangle.
-     * <-<-<-
-     * |    ^
-     * v    |
-     * ->->->
-     * When the square is complete, Karel moves to the inner rectangle.
-     * 1 1 1 1 1
-     * 1       1
-     * 1       1
-     * 1->     1
-     * 1 1 1 1 1
-     * Karel moves until he is in the center(or one of two) cell surrounded by beepers.
-     * 1 1 1 1 1
-     * 1 1 1 1 1
-     * 1 1-> 1 1
-     * 1 1 1 1 1
-     * 1 1 1 1 1
-     * And puts the last beeper in a centre cell (central column is found (or one of two))
-     */
-    private void fillEverythingWithBeepers() throws Exception {
-        while (!beepersPresent()) {
-            moveOneStepForwardAndPutBeeper();               // until the beeper is not found fill empty cells with beepers
-            if (beepersPresent()) {                         // If a beeper is found, the square is closed. You need to go to the internal one.
-                backToPreviousPosition();                   // back to the previous cell
-                takeNewDirection();                         // go to the inner square
-            }
-        }
+        /*turnBack();
+        move();*/
+        say("Job is finished!");
+
+
+
     }
 
     /**
      * Initialize Karel.
-     * Karel must say "Hello. I`m ready!" (switch on robot).
-     * Turn East.
+     * Karel must say "Hello. Ready to work." (switch on robot).
      */
-    private void initialize() throws Exception {
-        say("Hello. I`m ready to work!"); // switch on robot
-        while (!facingEast()){
-            turnLeft();                                     // turn until not looking East
-        }
-    }
-
-    /**
-     * Moves and puts beepers along the side of the rectangle.
-     */
-    private void moveOneStepForwardAndPutBeeper() throws Exception {
-        if(frontIsBlocked()){                               // if
-            turnLeft();                                     // Karel moves
-        }                                                   // along the border of the world
-        putBeeper();                                        // put a beeper
-        move();                                             // move to the next cell
-    }
-
-    /**
-     * Moves while the front is clear (movement to the border).
-     */
-    private void moveWhileFrontIsClear() throws Exception {
-        while (frontIsClear()){
-            move();                                         // moving until the front is clear
-        }
-    }
-
-    /**
-     * Returns to start position and puts second beeper at the middle of south line.
-     */
-    private void returnToStartPositionAndPutBeeper() throws Exception {
-        turnToSouth();                                      // turn South
-        moveWhileFrontIsClear();                            // move to the southern border
-        putBeeper();                                        // put second beeper
-        turnRight();                                        // turn West
-        moveWhileFrontIsClear();                            // move to the western border
-        turnBack();                                         // turn back for start of cleaning beepers
-    }
-
-    /**
-     * Takes a new direction. To move to the inner square.
-     */
-    private void takeNewDirection() throws Exception {
-        turnLeft();
-        move();
+    private void karelInit() throws Exception {
+        say("Hello. Ready to work.");
     }
 
     /**
@@ -179,23 +99,5 @@ public class Assignment1Part3 extends KarelTheRobot{
     private void turnBack() throws Exception {
         turnLeft();
         turnLeft();
-    }
-
-    /**
-     * Turn right.
-     */
-    private void turnRight() throws Exception {
-        for (int i = 0; i < 3; i++) {
-            turnLeft();
-        }
-    }
-
-    /**
-     * Turn South.
-     */
-    private void turnToSouth() throws Exception {
-        while (!facingSouth()){
-            turnLeft();                                     // turn left until look South
-        }
     }
 }
